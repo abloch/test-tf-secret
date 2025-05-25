7b5511439bcd2d0ce16417d12ec8d9a081385a9f
Akiva Bloch <akiva@kubiya.ai>
2025-05-25 12:53:35 +0300

    fix

--- simple/main.tf
+++ simple/main.tf
@@ -4,6 +4,16 @@ variable "SECRET1" {
 }
 
 output "secret1_output" {
-  description = "Output of variable v1"
+  description = "Output of variable secret1"
   value       = var.SECRET1
+}
+
+variable "stam" {
+  description = "Input variable secret1"
+  type        = string
+}
+
+output "stam" {
+  description = "Output of variable stam"
+  value       = var.stam
 }
